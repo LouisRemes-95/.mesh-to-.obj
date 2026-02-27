3eb94f015ca5858c3d68cf2046134f8cf2bf4260
Louis Remes <louisremes@gmail.com>
2026-02-27 18:52:24 +0100

    Proper convert to obj

--- src/mesh_to_obj/convert.py
+++ src/mesh_to_obj/convert.py
@@ -21,8 +21,12 @@ def convert(in_path, out_path: Path, data_label: str = None) -> None:
 
     _, inverse, counts = np.unique(np.sort(faces), axis = 0, return_inverse = True, return_counts = True)
     is_boundary_face = counts[inverse] == 1
-
     boundary_faces = faces[is_boundary_face, :]
+
+    points = np.unique(boundary_faces.flatten())
+
+    lut = np.full(points.max()+1, 0, dtype=smallest_uint_type(points.size))
+    lut[points] = np.arange(points.size)
 
     key = next(iter(mesh.cell_data), None)
     if key is not None:
@@ -30,4 +34,24 @@ def convert(in_path, out_path: Path, data_label: str = None) -> None:
 
         data = np.tile(mesh.cell_data_dict[data_label]['tetra'][:,None], (1, 4))
 
-    meshio.write(out_path, mesh, file_format="obj")
+    surface_mesh = meshio.Mesh(
+        points = mesh.points[points, :],
+        cells=[("triangle", lut[boundary_faces])]
+        )
+
+    meshio.write(out_path, surface_mesh, file_format="obj")
+
+def smallest_uint_type(x: int):
+    if x < 0:
+        raise ValueError("Unsigned integers cannot store negative values.")
+
+    if x <= np.iinfo(np.uint8).max:
+        return np.uint8
+    elif x <= np.iinfo(np.uint16).max:
+        return np.uint16
+    elif x <= np.iinfo(np.uint32).max:
+        return np.uint32
+    elif x <= np.iinfo(np.uint64).max:
+        return np.uint64
+    else:
+        raise ValueError("Integer too large for uint64.")
